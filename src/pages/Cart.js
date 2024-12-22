e28aa9b8ac86733f9c7959e0675027a2e06d9a0f
Lambert DONGFACK <dtlamberto@gmail.com>
2024-12-22 17:06:49 -0500

    first commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,10 +8,10 @@ export const Cart = () => {
 
   useTitle("Cart");
   
-  const products = [
-    {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png",}, 
-    {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png",}, 
-  ]
+  // const products = [
+  //   {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png",}, 
+  //   {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png",}, 
+  // ]
 
   return (
     <main>
